a9148e9bbd2c3dcc7787bd5ebb9ec2a20b8101aa
Zdravko Botushanov <zdravko7@abv.bg>
2016-05-05 15:06:43 +0300

    Started refactoring the code. Reordered files into folders, combined loging and register templates into 1 home template. Changed navbar template. Will refactor js files

--- app/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('issueTrackingSystem.version', [
-  'issueTrackingSystem.version.interpolate-filter',
-  'issueTrackingSystem.version.version-directive'
-])
-
-.value('version', '0.1');
